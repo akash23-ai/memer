8315b264c5918f1638c090db160f5ec0802ffa3e
akash23-ai <akashbhardwaj2024@gmail.com>
2024-02-27 12:18:41 +0530

    Added Dragable functionality to the code

--- src/components/mycode.tsx
+++ src/components/mycode.tsx
@@ -1,18 +1,16 @@
-"use client"
+"use client";
 
 // import Input from "@/components/ui/input";
 import React, { useEffect, useState } from "react";
 import { TextField } from "@mui/material";
-
+import Draggable from "react-draggable";
 
 interface Memes {
-  url : string,
-  name : string
+  url: string;
+  name: string;
 }
 
 export const Code = () => {
-
-
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
@@ -20,7 +18,6 @@ export const Code = () => {
   });
 
   const [memes, setMemes] = useState<Memes[]>([]);
-
 
   const canvasRef = React.createRef<HTMLCanvasElement>();
 
@@ -34,7 +31,6 @@ export const Code = () => {
 
     fetchMemes();
   }, []);
-
 
   const handleChange = (e: any) => {
     setMeme({
@@ -54,9 +50,7 @@ export const Code = () => {
     });
   };
 
-
-
-// For embeding the text on the image
+  // For embeding the text on the image
 
   // const imgLoad = () => {
   // const canvas = canvasRef.current;
@@ -70,10 +64,9 @@ export const Code = () => {
   // console.log(canvas);
   // }
 
-
-
   return (
     <div className="w-full flex justify-center items-center">
+     
       <div className="w-1/2 min-h-96 h-1/2 flex flex-col items-center">
         <TextField
           id="outlined-basic"
@@ -84,8 +77,8 @@ export const Code = () => {
           color="secondary"
           focused
           sx={{
-            input: { color: 'white' },
-            marginBottom: "2rem"
+            input: { color: "white" },
+            marginBottom: "2rem",
           }}
           className="w-1/2"
         />
@@ -96,20 +89,22 @@ export const Code = () => {
           onChange={handleChange}
           name="bottomText"
           sx={{
-            input: { color : "white"},
-            marginBottom: "2rem"
+            input: { color: "white" },
+            marginBottom: "2rem",
           }}
           focused
           className="w-1/2"
         />
 
-
-         { /* My tailwind button */}
+        {/* My tailwind button */}
         {/* <button className="bg-transparent border-2 hover:border-gray-900 border-slate-900/60 text-slate-900/65 active:border-blue-700/90 active:text-blue-700/80 hover:text-gray-900 border-dotted py-2 px-4 rounded-md font-medium leading-6" onClick={handleClick}>
           Next Meme
         </button> */}
 
-        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-100/95 active:text-blue-700/80 transition-colors focus:outline-none active:border-blue-700/40 hover:border-blue-600/50  focus:ring-offset-2 focus:ring-offset-slate-50" onClick={handleClick}>
+        <button
+          className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-100/95 active:text-blue-700/80 transition-colors focus:outline-none active:border-blue-700/40 hover:border-blue-600/50  focus:ring-offset-2 focus:ring-offset-slate-50"
+          onClick={handleClick}
+        >
           Next Meme
         </button>
 
@@ -120,17 +115,26 @@ export const Code = () => {
   </div>
 </button> */}
         <div className="mt-8 flex justify-center items-center w-full relative">
-          <span className="absolute z-10 top-4 leading-6 font-medium text-xl">
-            {meme.topText}
-          </span>
-          <img src={meme.img} alt="" className="w-[35rem] h-[25rem] contrast-125"/>
+          <Draggable>
+            <span className="absolute z-10 top-4 leading-6 font-medium text-xl cursor-pointer">
+              {meme.topText}
+            </span>
+          </Draggable>
+          <img
+            src={meme.img}
+            alt=""
+            className="w-[35rem] h-[25rem] contrast-125"
+          />
           {/* <canvas width="400" height="150" ref={canvasRef}></canvas> */}
-
-          <span className="absolute z-10 bottom-4 leading-6 font-medium text-xl">
-            {meme.bottomText}
-          </span>
+          <Draggable>
+            <span className="absolute z-10 bottom-4 leading-6 font-medium text-xl cursor-pointer">
+              {meme.bottomText}
+            </span>
+          </Draggable>
         </div>
+        
       </div>
+      <div className="absolute top-[0.06rem] right-1 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 font-semibold text-lg">You could Also Drag the Text wherever you Like</div>
     </div>
-  )
-}
+  );
+};
